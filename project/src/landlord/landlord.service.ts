5ee117d23a2b1511545d340fc09d7c62f326079a
ReDThunDeR33 <mdarko190@gmail.com>
2026-06-21 22:33:00 +0600

    4 get route

--- project/src/landlord/landlord.service.ts
+++ project/src/landlord/landlord.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LandlordService {}
+export class LandlordService {
+    
+  getLandlord(): string {
+    return 'All Landlord';
+  }
+
+  getAllLandlord(): object {
+    return { app: 'abc', id: '12' };
+  }
+
+  getLandlordByID(id: number, name: string): object {
+    return { name: name, id: id };
+  }
+
+  getLandlordByIDandName(id: number, name: string): object {
+    return { name: name, id: id };
+  }
+}

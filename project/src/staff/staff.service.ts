25a92f4579ce4f89a81acb2ba7441ab1f42e43cd
Arindom Paul <arindom793@gmail.com>
2026-07-05 23:06:48 +0600

    retry dto and staff database entity

--- project/src/staff/staff.service.ts
+++ project/src/staff/staff.service.ts
@@ -7,42 +7,4 @@ import { Repository } from 'typeorm';
 export class StaffService {
   constructor(@InjectRepository(staffData) private staffRepository: Repository<staffData>){}
 
-  async createStaff(staffData: staffData): Promise<staffData> {
-    return this.staffRepository.save(staffData);
-  }
-
-  async getAllStaffData(): Promise<staffData[]> {
-    return this.staffRepository.find();
-  }
-  async getStaffById(id: number): Promise<staffData | null> {
-    return this.staffRepository.findOneBy({ id: id });
-  }
-
-  async updateStaff( id: number, updateStaff: staffData): Promise<staffData | null> {
-    await this.staffRepository.update(id, updateStaff);
-    return this.staffRepository.findOneBy({ id: id });}
-
-  async deleteStaff(id: number){
-    await this.staffRepository.delete(id);
-  }
-
-    getAllStaff() {
-    return 'This will return all staff members.';
-  }
-
-  getStaffByQuery(id: string): object {
-    return { id: id, name: 'Alex', position: 'Staff Manager (Found via Query)' };
-  }
-
-  createUser(body: object): object {
-    return {
-      message: 'User created successfully'
-    }
-  }
-
-
-
-
-
-
 }

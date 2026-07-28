4099a9bb7059b9cface877a16daaaf50a8c258d8
ReDThunDeR33 <mdarko190@gmail.com>
2026-07-28 20:37:35 +0600

    .

--- project/src/landlord/landlord.module.ts
+++ project/src/landlord/landlord.module.ts
@@ -6,22 +6,10 @@ import { LandlordEntity } from './entities/landlord.entity';
 import { PropertyEntity } from './entities/property.entity';
 import {StaffModule} from "../staff/staff.module"
 import { workOrder } from 'src/staff/entities/work-order.entity';
-import { MailerModule } from 'node_modules/@nestjs-modules/mailer/dist/mailer.module';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LandlordEntity,PropertyEntity,workOrder]), StaffModule,MailerModule.forRoot({
-  transport: {
-  host: 'arko1920@gmail.com',
-  port: 465,
-  ignoreTLS: true,
-  secure: true,
-  auth: {
-  user: 'your gmail account',
-  pass: 'generated password'
-  },
-  }
-  })],
+  imports: [TypeOrmModule.forFeature([LandlordEntity,PropertyEntity,workOrder]), StaffModule,],
   controllers: [LandlordController],
   providers: [LandlordService],
   exports: [LandlordService],

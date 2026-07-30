d1d259a5a74abec7f04abb61bf29795d8747d201
Arindom <arindom793@gmail.com>
2026-07-30 11:29:46 +0600

    staff entity done

--- pms/src/staff/entities/staff.entity.ts
+++ pms/src/staff/entities/staff.entity.ts
@@ -1,1 +1,48 @@
-import {Entity} from ''
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn,ManyToOne, JoinColumn,OneToMany, Index } from 'typeorm';
+export enum UserStatus {
+    ACTIVE = "active",
+    DEACTIVE = "deactive",
+}
+
+
+@Entity('staff')
+export class StaffEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    @Index({ unique: true })
+    email: string;
+
+    @Column()
+    phone: string;
+
+    @Column()
+    password_hash: string;
+
+    //   @Column()
+    //   role?: string;
+
+    @Column({
+        type: "enum",
+        enum: UserStatus,
+        default: UserStatus.ACTIVE,
+    })
+    status: UserStatus;
+
+    @ManyToOne(() => Admin)
+    @JoinColumn({ name: 'created_by' })
+    created_by: Admin;
+
+
+    @CreateDateColumn()
+    created_at: Date;
+
+
+}
+
+
+
